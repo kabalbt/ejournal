f4dc504f685dc59b4da30d5d130bc3b54505a756
kabalbt <Kabalbt@gmail.com>
2026-02-28 09:29:57 +0100

    django views

--- common/views.py
+++ common/views.py
@@ -1,7 +1,9 @@
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+
+from common.tools import group_required
 
 
 # Create your views here.
@@ -36,7 +38,10 @@ def logout(request):
     return redirect('login')
 
 @login_required(login_url='login')
+#@user_passes_test(is_teacher_student_or_parent) способ через встроеный декорратор Django
+@group_required("Teacher", "Student", "Parent")
 def user_view(request, user_id):
     user = User.objects.get(id=user_id)
-    user_group = user.groups.all()[0]
-    return render(request, "user_page.html", context={"user": user, "group_name": user_group.name})
+    group_name = user.groups.first()
+    #user_group = user.groups.all()[0]
+    return render(request, "user_page.html", context={"user": user, "group_name": group_name})
